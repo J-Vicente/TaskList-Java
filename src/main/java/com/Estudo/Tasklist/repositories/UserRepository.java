799b05931a051223d9f52e1db46e823b780a1e6e
J-Vicente <joaovicent2713@gmail.com>
2025-09-04 21:15:39 -0300

    profiles criadas

--- src/main/java/com/Estudo/Tasklist/repositories/UserRepository.java
+++ src/main/java/com/Estudo/Tasklist/repositories/UserRepository.java
@@ -1,10 +1,10 @@
 package com.Estudo.Tasklist.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Estudo.Tasklist.entities.User;
-
-import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
